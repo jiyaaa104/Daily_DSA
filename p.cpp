b6e21724836a653c9248b16c0e71dc28fc1a92be
jiyaaa104 <snehal.codes117@gmail.com>
2026-07-02 16:35:42 +0530

    Word Search L-79 - My first 2D matrix Backtracking Problem

--- p.cpp
+++ p.cpp
@@ -1,135 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
-void generate(int n,string &current){
-   if(current.size()==n){
-      for(char ch: current){
-         cout<<ch;
-      }cout<<endl;
-      return;
+bool checkPalindrome(string&s, int start,int end){
+   string s1=s.substr(start,end-start+1);
+   string s2=s1;
+   start=0,end=s2.size()-1;
+   while(start<end){
+      swap(s2[start],s2[end]);
+      start++;end--;
    }
-   //pick 0
-   current.push_back('0');
-   generate(n,current);
-   current.pop_back();
-
-   //pick 1
-   current.push_back('1');
-   generate(n,current);
-   current.pop_back();
+   return (s2==s1);
 }
-void noConsecOnes(int n,string& current){
-   if(current.size() == n){
-      for(char ch: current){
-         cout<<ch;
-      }cout<<endl;
-      return;
-   }
-   //pick 0
-   current.push_back('0');
-   noConsecOnes(n,current);
-   current.pop_back();
-
-   //pick 1
-   if(current.empty() || current.back()!='1'){
-      current.push_back('1');
-      noConsecOnes(n,current);
-      current.pop_back();
-   }
-}
-void generateParenthesis(int n,int open,int close,string& current){
-   if(open==n && close==n){
-      cout<<current<<endl;
-      return;
-   }
-   if(open<n){
-      current.push_back('(');
-      generateParenthesis(n,open+1,close,current);
-      current.pop_back();
-   }
-   if(open>close){
-      current.push_back(')');
-      generateParenthesis(n,open,close+1,current);
-      current.pop_back();
-   }
-}
-void letterCasePerm(int index,string &s,string& current){
-    if(s.size()==current.size()){
-      cout<<current<<endl;
-      return;
+int palindromePartition(int index,string& s){
+   int cost=0,n=s.size();
+    if(index==s.size()){
+      return cost;
     }
-    if(isdigit(s[index])){
-      current.push_back(s[index]);
-      letterCasePerm(index+1,s,current);
-      current.pop_back();
-    }else{
-        current.push_back(tolower(s[index]));
-      letterCasePerm(index+1,s,current);
-      current.pop_back();
-
-      current.push_back(toupper(s[index]));
-      letterCasePerm(index+1,s,current);
-      current.pop_back();
-    }
-}
-void phoneNumber(int index,string &s,string &current,vector<string>&mapping){
-   if(index==s.size()){
-      cout<<current<<endl;
-      return;
-   }
-   int d=s[index]-'0';
-   string str=mapping[d];
-   for(int i=0;i<str.size();i++){
-      current.push_back(str[i]);
-      phoneNumber(index+1,s,current,mapping);
-      current.pop_back();
-   }
-}
-void combinationSum1(int index,vector<int>&candidates,int target,vector<int>&current,int sum){
-   if(index==candidates.size()){
-      if(target==0){
-      for(int x:current){
-         cout<<x<<" ";
-      }cout<<endl;
-   }
-   return;
-   }
-   if(candidates[index]<=target){
-      current.push_back(candidates[index]);
-      target-=candidates[index];
-      sum+=candidates[index];
-      combinationSum1(index,candidates,target,current,sum);
-      sum-=candidates[index];
-      target+=candidates[index];
-      current.pop_back();
-   }
-   combinationSum1(index+1,candidates,target,current,sum);
-}
-void combinationSum2(int index,vector<int>&candidates,int target,vector<int>&current,int sum){
-   if(target==0){
-      for(int x:current){
-         cout<<x<<" ";
-      }cout<<endl;
-      return;
-   }
-   for(int i=index;i<candidates.size();i++){
-      if(i>index && candidates[i]==candidates[i-1]) continue;
-      if(candidates[i]>target) break;
-      if(candidates[i]<=target){
-        current.push_back(candidates[i]);
-      target-=candidates[i];
-      sum+=candidates[i];
-      combinationSum1(i+1,candidates,target,current,sum);
-      sum-=candidates[i];
-      target+=candidates[i];
-      current.pop_back();
+    int leastCost=INT_MAX;
+    for(int i=index;i<n;i++){
+      bool check=checkPalindrome(s,index,i);
+      if(check){
+         cost=1+palindromePartition(i+1,s);
+         leastCost=min(leastCost,cost);
       }
-   }
+    }return leastCost;
 }
 int main(){
-   vector<string>mapping={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-   vector<int>current;
-   string s="23";
-   vector<int>a={2,3,5,6,7};
-   combinationSum2(0,a,7,current,0);
    return 0;
 }
--- recursionHard.cpp
+++ recursionHard.cpp
@@ -1,32 +1,55 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// bool isPalindrome(string s){
+//     string rev=s;
+//     int start=0,end=s.size()-1;
+//     while(start<end){
+//         swap(rev[start],rev[end]);
+//         start++;end--;
+//     }
+//     return (rev==s);
+// }
+// //This method is called front partitioning.. In this method at each index we go till the end and find the cost then return the least cost.This code returns the number of partitions, if you want to return the minimum number of cuts do partitions-1.
+// int palindromePartition2(int index,string &s){
+//     if(index==s.size()){
+//         return 0;
+//     }
+//     int minCost=INT_MAX;
+//     for(int j=index;j<s.size();j++){
+//         string  temp=s.substr(index,j-index+1);
+//         if(isPalindrome(temp)){
+//             int cost=1+palindromePartition2(j+1,s);
+//           minCost=min(cost,minCost);
+//         }
+//     }
+//     return minCost;
+// }
+// int main(){
+//     string s="bababcbadcede";
+//     string temp;
+//     cout<<palindromePartition2(0,s);
+//     return 0;
+// }
+
+//WORD SEARCH
+//LEETCODE -79
 #include<bits/stdc++.h>
 using namespace std;
-bool isPalindrome(string s){
-    string rev=s;
-    int start=0,end=s.size()-1;
-    while(start<end){
-        swap(rev[start],rev[end]);
-        start++;end--;
+bool wordSearch(int index,int x,int y,string& word,vector<vector<char>>&matrix){
+    if(index==word.size()){
+        return true;
     }
-    return (rev==s);
-}
-//This method is called front partitioning.. In this method at each index we go till the end and find the cost then return the least cost.This code returns the number of partitions, if you want to return the minimum number of cuts do partitions-1.
-int palindromePartition2(int index,string &s){
-    if(index==s.size()){
-        return 0;
+    if(x<0 || y<0 || x>=matrix.size() || y>=matrix[0].size() || matrix[x][y]!=word[index]){
+        return false;
     }
-    int minCost=INT_MAX;
-    for(int j=index;j<s.size();j++){
-        string  temp=s.substr(index,j-index+1);
-        if(isPalindrome(temp)){
-            int cost=1+palindromePartition2(j+1,s);
-          minCost=min(cost,minCost);
-        }
-    }
-    return minCost;
+    char temp=matrix[x][y];
+    matrix[x][y]='1';//visited
+    if(wordSearch(index+1,x+1,y,word,matrix)||wordSearch(index+1,x,y+1,word,matrix)||wordSearch(index+1,x-1,y,word,matrix)||wordSearch(index+1,x,y-1,word,matrix)){matrix[x][y]=temp;return true;} 
+    matrix[x][y]=temp;
+    return false;
 }
 int main(){
-    string s="bababcbadcede";
-    string temp;
-    cout<<palindromePartition2(0,s);
-    return 0;
+ vector<vector<char>>matrix={{'A','B','C'},{'D','E','F'},{'G','H','I'}};
+ string word="ABCFIH";
+ cout<<wordSearch(0,0,0,word,matrix);
 }
