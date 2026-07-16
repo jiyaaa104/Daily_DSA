d07718993bb08e63b46f3abfb52d9ecd8e2ef4bb
jiyaaa104 <snehal.codes117@gmail.com>
2026-07-16 12:33:17 +0530

    Understood in depth xor in a range. Solved L-1486 in O(1)

--- bitManipulationEasy.cpp
+++ bitManipulationEasy.cpp
@@ -68,7 +68,98 @@ void countSetBits(int n){
 int minimumBitFlips(int start,int goal){
     return __builtin_popcount(start^goal);
 }
+void printSubSets(vector<int>nums){
+    vector<vector<int>>ans;
+    int n=nums.size(),subsets=1<<n;
+    for(int num=0;num<subsets;num++){
+        vector<int>a;
+        for(int i=0;i<n;i++){
+           if(num&(1<<i)){
+            a.push_back(nums[i]);
+           }
+        }
+        ans.push_back(a);
+    }
+    for(int i=0;i<ans.size();i++){
+        for(int j=0;j<ans[i].size();j++){
+            cout<<ans[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+//SINGLE NUMBER 2 
+//APPROACH : Basically, we expand every number (integer) (32 bit places) Since a number always appears 3 and just one f them appears once. therefore For the answer's bit position at that particular index will be set if the number of set bits in that index is not a multiple of three otherwise it'll be 0
+int singleNumber2(vector<int>nums){
+    //METHOD 1:
+    // int n=nums.size();
+    // int place=1,ans=0;
+    // for(int i=0;i<31;i++){
+    //     int count=0;
+    //     for(int index=0;index<n;index++){
+    //       if(nums[index]&(1<<i))count++;
+    //     }
+    //     int rem=0;
+    //     if(count%3!=0){
+    //      rem=1;
+    //     }
+    //     ans=ans+place*rem;
+    //     place*=2;
+    // }
+    // return ans;
+    //METHOD 2:
+    // int n=nums.size();
+    // sort(nums.begin(),nums.end());
+    // int index=1,ans=nums[n-1];
+    // while(index<n){
+    //     if(nums[index]!=nums[index-1]){
+    //       ans=nums[index-1];
+    //       break;
+    //     }else{
+    //         index+=3;
+    //     }
+    // }
+    // return ans;
+    int once=0,twice=0,n=nums.size();
+    for(int i=0;i<n;i++){
+      once=(once^nums[i])&(~twice);
+      twice=(twice^nums[i])&(~once);
+    }return once;
+}
+void singleNumber3(vector<int>nums){
+    int n=nums.size();
+    long long x=0;
+    for(int i=0;i<n;i++){
+      x^=nums[i];
+    }
+    int value=(x&(x-1))^x;
+    int b1=0,b2=0;
+    for(int i=0;i<n;i++){
+        if(nums[i]&value){
+            b1^=nums[i];
+        }else{
+            b2^=nums[i];
+        }
+    }
+    cout<<b1<<" "<<b2<<endl;
+}
+int xor1N(int n){
+    switch(n%4){
+        case 0: return n;
+        case 1: return 1;
+        case 2: return n+1;
+        default: return 0;
+    }
+}
+int xorRange(int l,int r){
+    return xor1N(r)^xor1N(l-1);
+}
+int xor1486(int n,int start){
+    int s=start>>1;
+    return (xorRange(s,s+n-1)<<1)|((start&1)&(n&1));
+}
 int main(){
-   countSetBits(13);
+//     vector<int>nums={1,2,1,3,2,5};
+//    singleNumber3(nums);5^7^9^11^13
+cout<<xor1486(5,12);
     return 0;
 }
--- p.cpp
+++ p.cpp
@@ -1,108 +1,59 @@
 #include<bits/stdc++.h>
 using namespace std;
-void permute(int index,string&s,string&current){
-    if(index==s.size()){
-        cout<<current<<" ";
-        return;
-    }
-    if(isdigit(s[index])){
-        current.push_back(s[index]);
-        permute(index+1,s,current);
-        current.pop_back();
-    }
-    else{
-        current.push_back(tolower(s[index]));
-        permute(index+1,s,current);
-        current.pop_back();
-        current.push_back(toupper(s[index]));
-        permute(index+1,s,current);
-        current.pop_back();
-    }
-}
-void combination(int index,string&digits,string & current,vector<string>&phone){
-    if(index==digits.size()){
-        cout<<current<<endl;
-        return;
-    }
-    int i=digits[index]-'0';
-    string p=phone[i];
-    for(int i=0;i<p.size();i++){
-        current.push_back(p[i]);
-        combination(index+1,digits,current,phone);
-        current.pop_back();
-    }
-}
-void cSum(int index,vector<int>&candidates,int target,vector<int>&current,int sum){
-    if(index==candidates.size()){
-        if(target==0){
-            for(int x: current){
-                cout<<x<<" ";
-            }cout<<endl;
-        }
-        return;
-    }
-    if(candidates[index]<=target){
-        sum+=candidates[index];
-        target-=candidates[index];
-        current.push_back(candidates[index]);
-        cSum(index,candidates,target,current,sum);
-        current.pop_back();
-        target+=candidates[index];
-        sum-=candidates[index];
-    }
-    cSum(index+1,candidates,target,current,sum);
-}
-void cSum2(int index,vector<int>&candidates,int target,vector<int>&current,int sum){
-    if(target==0){
-        for(int x: current){
-            cout<<x<<" ";
-        }cout<<endl;
-        return;
-    }
-    for(int i=index;i<candidates.size();i++){
-        if(i>index && candidates[i]==candidates[i-1]) continue;
-        if(candidates[i]>target){
-            break;
-        }
-        sum+=candidates[i];
-        target-=candidates[i];
-        current.push_back(candidates[i]);
-        cSum2(i+1,candidates,target,current,sum);
-        current.pop_back();
-        target+=candidates[i];
-        sum-=candidates[i];
-    }
-}
-void cSum3(int index,vector<int>&current,int target,int k){
-    if(current.size()>k) return;
-    if(current.size()==k){
-        if(target==0){
-            for(int x: current){
-                cout<<x<<" ";
+void subsets(vector<int>nums){
+    int n=nums.size();
+    int subset=1<<n;
+    vector<vector<int>>ans;
+    for(int i=0;i<subset;i++){
+        vector<int>a;
+        for(int index=0;index<n;index++){
+            if(i&(1<<index)){
+              a.push_back(nums[index]);
             }
-            cout<<endl;
         }
-        return;
+        ans.push_back(a);
     }
-    for(int i=index;i<=9;i++){
-        if(i>target) break;
-        target-=i;
-        current.push_back(i);
-        cSum3(i+1,current,target,k);
-        target+=i;
-        current.pop_back();
+    for(int i=0;i<ans.size();i++){
+        for(int j=0;j<ans[i].size();j++){
+            cout<<ans[i][j]<<" ";
+        }cout<<endl;
     }
+}
+int singleNumber(vector<int>nums){
+    // int n=nums.size();
+    // long long ans=0,place=1;
+    // for(int i=0;i<32;i++){
+    //     int count=0;
+    //     for(int index=0;index<n;index++){
+    //         if(nums[index]&(1<<i))count++;
+    //     }
+    //     int rem=0;
+    //     if(count%3!=0){
+    //         rem=1;
+    //     }
+    //     ans=ans+place*rem;
+    //     place*=2;
+    // }
+    // return ans;
+    // sort(nums.begin(),nums.end());
+    // int n=nums.size();
+    // int ans=nums[n-1];
+    // int index=1;
+    // while(index<n){
+    //     if(nums[index]!=nums[index-1]){
+    //         ans=nums[index];
+    //         break;
+    //     }
+    //     index+=3;
+    // }return ans;
+    int once=0,twice=0;
+    for(int i=0;i<nums.size();i++){
+        once=(once^nums[i])&(~twice);
+        twice=(twice^nums[i])&(~once);
+    }return once;
 }
 int main(){
-    vector<string>phone={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-    string s="a1b2";
-   vector<int>current;
-    string digits="23";
-    vector<int>candidates={2,2,2,1,3,6,7};
-    int sum=0,target=7;
-    // cSum2(0,candidates,target,current,sum);
-    cSum3(1,current,9,3);
-    // cSum(0,candidates,target,current,sum);
-    // combination(0,digits,current,phone);
+    vector<int>nums={1,2,3,1,2,1,2};
+    cout<<singleNumber(nums);
     return 0;
 }
