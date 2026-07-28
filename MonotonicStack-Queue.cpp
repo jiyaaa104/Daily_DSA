7db8610d96e940237590e612ee0385676e8998f9
jiyaaa104 <snehal.codes117@gmail.com>
2026-07-28 11:55:51 +0530

    Next Greater Element 3 -L-503

--- MonotonicStack-Queue.cpp
+++ MonotonicStack-Queue.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 //1. NEXT GREATER ELEMENT
-void nextGreaterElement(vector<int>arr){
+void nextGreaterElement1(vector<int>arr){
     int n=arr.size();
     vector<int>ans(n);
     stack<int>st;
@@ -24,8 +24,31 @@ void nextGreaterElement(vector<int>arr){
         cout<<ans[i]<<" ";
     }cout<<endl;
 }
+//L-503
+void nextGreaterElement2(vector<int>arr){
+    int n=arr.size();
+   vector<int>ans(n);stack<int>st;
+   for(int i=2*n-1;i>=0;i--){
+    if(st.empty()){
+      ans[i%n]=-1;
+    }else{
+        while(!st.empty() && st.top()<=arr[i%n]){
+            st.pop();
+        }
+        if(st.empty()){
+            ans[i%n]=-1;
+        }else{
+            ans[i%n]=st.top();
+        }
+    }
+    st.push(arr[i%n]);
+   }
+   for(int i=0;i<n;i++){
+    cout<<ans[i]<<" ";
+   }
+}
 int main(){
-    vector<int>arr={4,12,5,3,1,2,5,3,1,2,4,6};
-    nextGreaterElement(arr);
+    vector<int>arr={1,2,4,3,1};
+    nextGreaterElement2(arr);
     return 0;
 }
